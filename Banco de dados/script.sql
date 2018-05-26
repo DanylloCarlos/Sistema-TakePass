263e2e43310316ec6be145399a956988352d5801
Danyllo carlos <danyllo.rsilva@gmail.com>
2018-05-26 05:13:25 -0300

    Adição da tabela Evento como associativa
    A tabela Evento precisou ser adicionada pois a estrutura do DAO ficaria comprometida. Renomeação do nome do arquivo mwb e adição do arquivo de script, atualizado com a tabela Evento em bdEventos.

---
+++ Banco de dados/script.sql
@@ -0,0 +1,83 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema bdEventos
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `bdEventos` ;
+
+-- -----------------------------------------------------
+-- Schema bdEventos
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bdEventos` DEFAULT CHARACTER SET utf8 ;
+SHOW WARNINGS;
+USE `bdEventos` ;
+
+-- -----------------------------------------------------
+-- Table `bdEventos`.`Clientes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bdEventos`.`Clientes` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `bdEventos`.`Clientes` (
+  `idCliente` INT NOT NULL AUTO_INCREMENT,
+  `nomeCliente` VARCHAR(50) NULL,
+  `cpf` VARCHAR(45) NULL,
+  PRIMARY KEY (`idCliente`))
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `bdEventos`.`Ingressos`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bdEventos`.`Ingressos` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `bdEventos`.`Ingressos` (
+  `idIngresso` INT NOT NULL AUTO_INCREMENT,
+  `codEvento` VARCHAR(50) NULL,
+  `nomeEvento` VARCHAR(45) NULL,
+  PRIMARY KEY (`idIngresso`))
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `bdEventos`.`Evento`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `bdEventos`.`Evento` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `bdEventos`.`Evento` (
+  `idEvento` INT NOT NULL,
+  `nomeEvento` VARCHAR(45) NULL,
+  `Clientes_idCliente` INT NOT NULL,
+  `Ingressos_idIngresso` INT NOT NULL,
+  PRIMARY KEY (`idEvento`),
+  CONSTRAINT `fk_Evento_Clientes`
+    FOREIGN KEY (`Clientes_idCliente`)
+    REFERENCES `bdEventos`.`Clientes` (`idCliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Evento_Ingressos1`
+    FOREIGN KEY (`Ingressos_idIngresso`)
+    REFERENCES `bdEventos`.`Ingressos` (`idIngresso`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_Evento_Clientes_idx` ON `bdEventos`.`Evento` (`Clientes_idCliente` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `fk_Evento_Ingressos1_idx` ON `bdEventos`.`Evento` (`Ingressos_idIngresso` ASC);
+
+SHOW WARNINGS;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
