6b2014dc1047590930653904270efe8fc8276992
danyllocarlos <danyllo.rsilva@gmail.com>
2018-08-01 18:57:40 -0300

    Adicionando tratamento de exceção em todos os métodos da classe

--- Aplicação/AppServidor/src/dao/EventoDAO.java
+++ Aplicação/AppServidor/src/dao/EventoDAO.java
@@ -18,63 +18,88 @@ public class EventoDAO {
 	private String sql;
 	private ArrayList<Clientes> listaDeClientesPorEvento;
 	
-	public EventoDAO() throws ClassNotFoundException,SQLException{
-		c = ConexaoBD.novaConexao();
+	public EventoDAO() {
+		
+		try {
+			c = ConexaoBD.novaConexao();
+		
+		}catch(SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
 	}
 	
-	public void cadastrarEvento(String nomeDoEvento, int qtdIngressos, int idCliente) throws SQLException{
+	public void cadastrarEvento(String nomeDoEvento, int qtdIngressos, int idCliente){
 		
 		sql = "Insert into Eventos(nomeEvento, qtdIngressos)"
 				+ " values (?, ?)";
 		
-		pstm = c.prepareStatement(sql);
-		pstm.setString(1, nomeDoEvento);
-		pstm.setInt(2, qtdIngressos);
-		
-		pstm.executeUpdate();
-		
-		pstm.close();
-		c.close();
+		try {
+			
+			pstm = c.prepareStatement(sql);
+			pstm.setString(1, nomeDoEvento);
+			pstm.setInt(2, qtdIngressos);
+			
+			pstm.executeUpdate();
+			
+			pstm.close();
+			c.close();
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
 	}
 	
-	public void removerEvento(Eventos e) throws SQLException {
+	public void removerEvento(Eventos e){
 		
 		String sql = "Delete from Eventos e where e.IdEvento = ?";
 		
-		pstm = c.prepareStatement(sql);
-		pstm.setInt(1, e.getIdEvento());
-		
-		pstm.execute();
-		
-		pstm.close();
-		c.close();
+		try {
+			pstm = c.prepareStatement(sql);
+			pstm.setInt(1, e.getIdEvento());
+			
+			pstm.execute();
+			
+			pstm.close();
+			c.close();
+			
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
+		}
 		
 		
 	}
 	
-	public Eventos buscarEvento(int idEvento) throws SQLException {
+	public Eventos buscarEvento(int idEvento){
 		
 		Eventos e = new Eventos();
 		
 		String sql = "Select * from Eventos e where e.idEvento =?";
 		
-		pstm = c.prepareStatement(sql);
-		pstm.setInt(1, idEvento);
-		rs = pstm.executeQuery();
+		try {
+			pstm = c.prepareStatement(sql);
+			pstm.setInt(1, idEvento);
+			rs = pstm.executeQuery();
+			
+			pstm.close();
+			c.close();
+			
+			while(rs.next()) {
+				e.setIdEvento(rs.getInt("idEvento"));
+				e.setNomeEvento(rs.getString("nomeEvento"));
+				e.setClientes_idCliente(rs.getInt("Clientes_idCliente"));
+				e.setIngressos_idIngresso(rs.getInt("Ingressos_idIngresso"));
+			}
+			
+			return e;
 		
-		pstm.close();
-		c.close();
-		
-		while(rs.next()) {
-			e.setIdEvento(rs.getInt("idEvento"));
-			e.setNomeEvento(rs.getString("nomeEvento"));
-			e.setClientes_idCliente(rs.getInt("Clientes_idCliente"));
-			e.setIngressos_idIngresso(rs.getInt("Ingressos_idIngresso"));
+		}catch(SQLException sqe) {
+			sqe.printStackTrace();
 		}
-		return e;
+		
+		return null;
 	}
 	
-	public ArrayList<Clientes> listarClientesPorEvento(int codigoDeAcesso) throws SQLException{
+	public ArrayList<Clientes> listarClientesPorEvento(int codigoDeAcesso) {
 		listaDeClientesPorEvento = new ArrayList<>();
 		
 		sql = "Select nomeCliente, cpf from Clientes c, Eventos e "
